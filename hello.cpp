09525cd0c7e5f0d81758e2b004dbee15b22835e2
Glitched Da Kitty Cat School Account <delanceycal@mvr3.org>
2024-10-21 11:06:41 -0500

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int developercode = 282024;
+    cout << "Hello, World!\n";
+    cout << "This Is My First Working C++ Program\n";
+    cout << "Developer Code: " << "Cal" << developercode << "\n";
+    cout << "Developer Code Translated To Human: Glitched Da Kitty Cat";
+    return 0;
+}
